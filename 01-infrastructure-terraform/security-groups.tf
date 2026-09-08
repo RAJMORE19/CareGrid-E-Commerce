1b5916094ed117df2521543db79ba78aec913126
Raj More | DevOps / Cloud Engineer <167281073+RAJMORE19@users.noreply.github.com>
2026-09-08 11:01:56 +0530

    Update security-groups.tf

--- 01-infrastructure-terraform/security-groups.tf
+++ 01-infrastructure-terraform/security-groups.tf
@@ -1,1 +1,25 @@
+resource "aws_security_group" "additional_node_sg" {
+  name        = "${var.cluster_name}-additional-node-sg"
+  description = "Additional network controls for CareGrid EKS worker nodes."
+  vpc_id      = module.vpc.vpc_id
 
+  ingress {
+    description = "Intra-cluster communication for cross-pod container networking"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    self        = true
+  }
+
+  egress {
+    description = "Outbound egress rule to allow image pulls, updates, and external API requests"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "${var.cluster_name}-node-sg"
+  }
+}
