4df4bfe67e0fba443e79f98175f7ae7db4bf4076
Raj More | DevOps / Cloud Engineer <167281073+RAJMORE19@users.noreply.github.com>
2026-09-19 20:41:10 +0530

    Update versions.tf

--- 01-infrastructure-terraform/versions.tf
+++ 01-infrastructure-terraform/versions.tf
@@ -1,25 +1,12 @@
 terraform {
-  required_version = ">= 1.5.0"
+  required_version = ">= 1.6.0"
 
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.40"
-    }
-    tls = {
-      source  = "hashicorp/tls"
-      version = "~> 4.0"
+      version = "~> 6.0"
     }
   }
-
-  # For remote state management with state locking (Production Best Practice)
-  # backend "s3" {
-  #   bucket         = "caregrid-terraform-state-backend"
-  #   key            = "environments/dev/terraform.tfstate"
-  #   region         = "ap-south-1"
-  #   dynamodb_table = "caregrid-terraform-locks"
-  #   encrypt        = true
-  # }
 }
 
 provider "aws" {
@@ -27,10 +14,9 @@ provider "aws" {
 
   default_tags {
     tags = {
-      Project     = "CareGrid-E-Commerce"
+      Project     = var.project_name
       Environment = var.environment
       ManagedBy   = "Terraform"
-      Repository  = "https://github.com/RAJMORE19/CareGrid-E-Commerce"
     }
   }
 }
